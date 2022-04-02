afa74581088de415d1402d6399a2393a05aa3741
Alexander Vetrenko <vetrenko.ag@gmail.com>
2022-04-02 16:47:33 +0500

    Первая отладка

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@ string a, b, c, d, e;
 
 Input(out a, out b, out c, out d, out e);
 string[] matrix = {a, b, c, d, e};
+PrintArray(matrix);
+System.Console.Write(" => ");
 PrintArray(ShortMassiv(matrix));
-/*FillArray(matrix, x, y);
-System.Console.WriteLine();
-PrintArray(matrix, x, y); */
+
 
 void Input(out string a1, out string a2, out string a3, out string a4, out string a5)
 {
-    System.Console.WriteLine("Наполните маасив данными. Необходимо ввести пять любых слов (необязательно осмысленных), после ввода каждого нажимайте Enter:");
+    System.Console.WriteLine("Введите пять любых слов (необязательно осмысленных), после ввода каждого нажимайте Enter:");
     a1 = Console.ReadLine();
     a2 = Console.ReadLine();
     a3 = Console.ReadLine();
@@ -44,21 +44,11 @@ string[] ShortMassiv(string[] arr)
 
 void PrintArray(string[] arr)
 {
+    System.Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         System.Console.Write($"{arr[i],3} |");
     }
+    System.Console.Write("]");
 }
-
-/*void FillArray(int[,] arr, int a, int b)
-
-{
-    for (int i = 0; i < a; i++)
-    {
-        for (int j = 0; j < b; j++)
-        {
-            arr[i, j] = new Random().Next(1, 100);
-        }
-    }
-} */
 
